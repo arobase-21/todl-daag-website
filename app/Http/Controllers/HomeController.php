29d98c4660ec1ff57af2b670f49469957880200e
Samtou Christophe ASSEKOUDA <christophe1999da@gmail.com>
2023-05-03 22:00:47 +0000

    feat: Create HomeController

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request): View|Application|Factory
+    {
+        return view('home');
+    }
+}
