88c6de841fb11f36be4e9461a723c543f85d5f8c
Samtou Christophe ASSEKOUDA <christophe1999da@gmail.com>
2023-05-04 10:05:44 +0000

    feat: Add Service and Event Controllers

---
+++ app/Http/Controllers/ServiceController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
+
+class ServiceController extends Controller
+{
+    private array $services;
+    public function __construct()
+    {
+        $this->services = ['courtage', 'conseil', 'formation', 'builder'];
+    }
+
+    public function index(Request $request): View|Application|Factory
+    {
+        return view('services.index');
+    }
+
+    public function show(Request $request, string $slug): View|Application|Factory|\Illuminate\Contracts\Foundation\Application
+    {
+        if (!in_array($slug, $this->services)) {
+            return abort(404);
+        }
+        return view('services.' . $slug);
+    }
+}
