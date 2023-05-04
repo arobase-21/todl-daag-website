1982f855fc9a3e48ab1da35de9443f85dddf28e3
Samtou Christophe ASSEKOUDA <christophe1999da@gmail.com>
2023-05-04 12:46:05 +0000

    feat: Update homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,7 +12,7 @@
                                 <div>
                                     <h2 class="font-primary text-transform-none">2ᵉ édition de la Conférence sur l'Entrepreneuriat Agricole</h2>
                                     <p class="fw-light font-primary d-none d-sm-block">Rencontrez des experts du domaine, échangez des idées et découvrez comment lancer et développer une entreprise agricole prospère. Inscrivez-vous dès maintenant !</p>
-                                    <a href="#" class="button button-rounded button-large text-transform-none ls-0 font-primary">S'inscrire</a>
+                                    <a href="https://l.facebook.com/l.php?u=https%3A%2F%2Fforms.gle%2F2PwH8WZzGzv1KUki9%3Ffbclid%3DIwAR3LN6LtTRi2mE7gKTylUJRVd2me-iaABnAHmtAozFxlXDJeT469QGTAwew&h=AT08A2oz-Fn7x0QsAqoMPUWlO3iDaYbWuddFxZvKY4IPum7mGFKnPtEyio6iQTSxiW47WSEMoA2hl1pobNdhkBEjGtduOnmitvaPPOlldlyBRmSUv4T7g8HxzpMZ2CRtmj7e" class="button button-rounded button-large text-transform-none ls-0 font-primary">S'inscrire</a>
                                 </div>
                             </div>
                         </div>
@@ -24,7 +24,7 @@
                                 <div>
                                     <h2 class="font-primary">Une passion pour l'agriculture et l'entrepreneuriat.</h2>
                                     <p class="fw-light font-primary d-none d-sm-block"> Nous croyons que ces deux domaines sont étroitement liés et qu'ils ont un rôle important à jouer dans l'avenir de notre économie et de notre société.</p>
-                                    <a href="#" class="button button-rounded button-large text-transform-none ls-0 font-primary">Nous rejoindre</a>
+                                    <a href="{{ route('contacts') }}" class="button button-rounded button-large text-transform-none ls-0 font-primary">Nous rejoindre</a>
                                 </div>
                             </div>
                         </div>
@@ -36,21 +36,18 @@
                                 <div>
                                     <h2 class="font-primary">Développez vos compétences en entrepreneuriat agricole</h2>
                                     <p class="fw-light font-primary d-none d-sm-block">Nos formations vous préparent à réussir dans l'industrie agricole et à faire prospérer votre entreprise.</p>
-                                    <a href="#" class="button button-rounded button-large text-transform-none ls-0 font-primary">Nous contacter</a>
+                                    <a href="{{ route('contacts') }}" class="button button-rounded button-large text-transform-none ls-0 font-primary">Nous contacter</a>
                                 </div>
                             </div>
                         </div>
                         <div class="swiper-slide-bg" style="background-image: url('{{ asset('demos/business/images/slider/3.png') }}');"></div>
                     </div>
                 </div>
-                <!-- Slider Arrows
-                ============================================= -->
+
                 <div class="slider-arrow-left"><i class="bi-arrow-left"></i></div>
                 <div class="slider-arrow-right"><i class="bi-arrow-right"></i></div>
             </div>
 
-            <!-- Slider Mouse Icon
-            ============================================= -->
             <a href="#" data-scrollto="#content" data-offset="0" class="dark one-page-arrow">
                 <img class="infinite animated fadeInDown" src="{{ asset('demos/business/images/mouse.svg') }}" alt="Image" >
             </a>
@@ -70,61 +67,25 @@
                     </div>
 
                     <div class="row align-items-stretch g-4">
-                        <div class="col-lg-3 col-md-6">
-                            <div class="feature-box media-box fbox-bg h-100">
-                                <div class="fbox-media">
-                                    <a href="service-courtage.html">
-                                        <img src="{{ asset('demos/pet/images/services/1.jpg') }}" alt="Featured Box Image">
-                                    </a>
-                                </div>
-                                <div class="fbox-content border-0">
-                                    <h3 class="text-transform-none ls-0 fw-semibold">
-                                        Formations et Accompagnement<span class="subtitle fw-light ls-0">Globally parallel task premium infomediaries</span>
-                                    </h3>
-                                    <a href="service-courtage.html" class="button-link border-0 color">En savoir plus</a>
-                                </div>
-                            </div>
-                        </div>
 
-                        <div class="col-lg-3 col-md-6">
-                            <div class="feature-box media-box fbox-bg h-100">
-                                <div class="fbox-media">
-                                    <a href="#">
-                                        <img src="{{ asset('demos/pet/images/services/2.jpg') }}" alt="Featured Box Image">
-                                    </a>
-                                </div>
-                                <div class="fbox-content border-0">
-                                    <h3 class="text-transform-none ls-0 fw-semibold">
-                                        Courtage Agricole<span class="subtitle fw-light ls-0">Energistically visualize market-driven.</span>
-                                    </h3>
-                                    <a href="#" class="button-link border-0 color">En savoir plus</a>
+                        @foreach($companyServices as $service)
+                            <div class="col-lg-3 col-md-6">
+                                <div class="feature-box media-box fbox-bg h-100">
+                                    <div class="fbox-media">
+                                        <a href="{{ $service['url'] }}">
+                                            <img src="{{ $service['image'] }}" alt="Featured Box Image">
+                                        </a>
+                                    </div>
+                                    <div class="fbox-content border-0">
+                                        <h3 class="text-transform-none ls-0 fw-semibold">
+                                            {{ $service['title'] }}<span class="subtitle fw-light ls-0">{{ $service['description'] }}</span>
+                                        </h3>
+                                        <a href="{{ $service['url'] }}" class="button-link border-0 color">En savoir plus</a>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
+                        @endforeach
 
-                        <div class="col-lg-3 col-md-6">
-                            <div class="feature-box media-box fbox-bg h-100">
-                                <div class="fbox-media">
-                                    <a href="#"><img src="{{ asset('demos/pet/images/services/3.jpg') }}" alt="Featured Box Image"></a>
-                                </div>
-                                <div class="fbox-content border-0">
-                                    <h3 class="text-transform-none ls-0 fw-semibold">Conseils Agricoles<span class="subtitle fw-light ls-0">Enthusiastically iterate enabled portals after.</span></h3>
-                                    <a href="#" class="button-link border-0 color">En savoir plus</a>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col-lg-3 col-md-6">
-                            <div class="feature-box media-box fbox-bg h-100">
-                                <div class="fbox-media">
-                                    <a href="#"><img src="{{ asset('demos/pet/images/services/4.jpg') }}" alt="Featured Box Image"></a>
-                                </div>
-                                <div class="fbox-content border-0">
-                                    <h3 class="text-transform-none ls-0 fw-semibold">Builder<span class="subtitle fw-light ls-0">Enthusiastically iterate enabled portals after.</span></h3>
-                                    <a href="#" class="button-link border-0 color">En savoir plus</a>
-                                </div>
-                            </div>
-                        </div>
                     </div>
                 </div>
             </div>
@@ -132,7 +93,7 @@
 
             <div class="section p-0 mt-0 bg-transparent" style="border-top: 1px solid #EEE; border-bottom: 1px solid #EEE;">
                 <div class="row align-items-stretch">
-                    <div class="col-lg-6 text-center col-padding" style="background: url('{{ asset('demos/business/images/sections/1.jpg') }}') center center no-repeat; background-size: cover;">
+                    <div class="col-lg-6 text-center col-padding" style="background: url('{{ asset('demos/business/images/sections/1.png') }}') center center no-repeat; background-size: cover;">
                         <div>&nbsp;</div>
                     </div>
 
@@ -144,8 +105,6 @@
                                 </div>
                             </div>
 
-                            <!-- About Us Featured Boxes
-                            ============================================= -->
                             <div class="row g-5">
                                 <div class="col-lg-10 col-md-8">
                                     <div class="feature-box fbox-plain">
@@ -153,8 +112,11 @@
                                             <a href="#"><i class="icon-et-globe"></i></a>
                                         </div>
                                         <div class="fbox-content">
-                                            <h3>All over the World</h3>
-                                            <p>Powerful Layout with Responsive functionality that can be adapted to any screen size. Resize browser to view.</p>
+                                            <h3>Expérience et expertise</h3>
+                                            <p>
+                                                Notre entreprise possède une grande expérience et une expertise approfondie dans le domaine de l'entrepreneuriat agricole.
+                                                Nous sommes des professionnels passionnés et dévoués qui sont engagés à aider nos clients à réussir dans l'industrie agricole.
+                                            </p>
                                         </div>
                                     </div>
                                 </div>
@@ -164,8 +126,12 @@
                                             <a href="#"><i class="icon-et-gears"></i></a>
                                         </div>
                                         <div class="fbox-content">
-                                            <h3>Fully Customisable</h3>
-                                            <p>Powerful Layout with Responsive functionality that can be adapted to any screen size. Resize browser to view.</p>
+                                            <h3>Approche personnalisée</h3>
+                                            <p>
+                                                Nous croyons que chaque client est unique et mérite une approche personnalisée.
+                                                Nous travaillons en étroite collaboration avec nos clients pour comprendre leurs besoins
+                                                spécifiques et élaborer des solutions sur mesure qui répondent à leurs besoins.
+                                            </p>
                                         </div>
                                     </div>
                                 </div>
@@ -175,8 +141,12 @@
                                             <a href="#"><i class="icon-et-document"></i></a>
                                         </div>
                                         <div class="fbox-content">
-                                            <h3>Helpfull Documentation</h3>
-                                            <p>Powerful Layout with Responsive functionality that can be adapted to any screen size. Resize browser to view.</p>
+                                            <h3>Engagement envers la durabilité</h3>
+                                            <p>
+                                                Nous sommes convaincus que l'agriculture durable est la clé de l'avenir de l'industrie agricole.
+                                                C'est pourquoi nous nous engageons à promouvoir des pratiques agricoles durables et à soutenir
+                                                les agriculteurs qui cherchent à adopter des pratiques agricoles respectueuses de l'environnement.
+                                            </p>
                                         </div>
                                     </div>
                                 </div>
@@ -187,44 +157,62 @@
                 </div>
             </div>
 
-            <div class="container py-6">
+            <div class="container py-6" style="
+                margin-left: 0;
+                margin-right: 0;
+                width: 100%;
+                max-width: unset;
+            ">
                 <div class="row justify-content-center">
                     <div class="col-md-10">
                         <div class="row col-mb-30">
-                            <div class="col-md-4">
+                            <div class="col-md-4" style="max-width: 375px">
                                 <div class="feature-box fbox-small fbox-effect">
                                     <div class="fbox-icon mb-4 mt-3">
-                                        <a href="#"><i class="bi-calendar"></i></a>
+                                        <i class="bi-building-gear"></i>
                                     </div>
                                     <div class="fbox-content">
-                                        <div class="counter"><span data-from="1" data-to="15" data-refresh-interval="10" data-speed="1600"></span>+</div>
+                                        <div class="counter"><span data-from="1" data-to="15" data-refresh-interval="3" data-speed="600"></span>+</div>
                                         <h3>Cooperative agricole partenaire.</h3>
                                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, quod consectetur.</p>
                                     </div>
                                 </div>
                             </div>
 
-                            <div class="col-md-4">
+                            <div class="col-md-4" style="max-width: 375px">
                                 <div class="feature-box fbox-small fbox-effect">
                                     <div class="fbox-icon mb-4 mt-3">
-                                        <a href="#"><i class="bi-file-earmark-richtext"></i></a>
+                                        <i class="bi-truck"></i>
                                     </div>
                                     <div class="fbox-content">
-                                        <div class="counter"><span data-from="4" data-to="83" data-refresh-interval="50" data-speed="1500"></span>%</div>
-                                        <h3>of Works Completed.</h3>
+                                        <div class="counter"><span data-from="100" data-to="3000" data-refresh-interval="300" data-speed="900"></span>+</div>
+                                        <h3>Tonnes exportées</h3>
                                         <p>Lorem ipsum dolor, sit, amet consectetur adipisicing elit. Beatae impedit, dignissimos vitae!</p>
                                     </div>
                                 </div>
                             </div>
 
-                            <div class="col-md-4">
+                            <div class="col-md-4" style="max-width: 375px">
                                 <div class="feature-box fbox-small fbox-effect">
                                     <div class="fbox-icon mb-4 mt-3">
-                                        <a href="#"><i class="bi-people"></i></a>
+                                        <i class="bi-people"></i>
                                     </div>
                                     <div class="fbox-content">
-                                        <div class="counter"><span data-from="5" data-to="100" data-refresh-interval="30" data-speed="1200"></span>%</div>
-                                        <h3>Satisfied Customers.</h3>
+                                        <div class="counter"><span data-from="100" data-to="5000" data-refresh-interval="100" data-speed="1000"></span>+</div>
+                                        <h3>Producteurs partenaires</h3>
+                                        <p>Amet consectetur adipisicing elit. Accusantium dolores magnam ab consequuntur, impedit.</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-md-4" style="max-width: 375px">
+                                <div class="feature-box fbox-small fbox-effect">
+                                    <div class="fbox-icon mb-4 mt-3">
+                                        <i class="bi-calendar"></i>
+                                    </div>
+                                    <div class="fbox-content">
+                                        <div class="counter"><span data-from="1" data-to="8" data-refresh-interval="2" data-speed="300"></span>+</div>
+                                        <h3>Années d'expérience</h3>
                                         <p>Amet consectetur adipisicing elit. Accusantium dolores magnam ab consequuntur, impedit.</p>
                                     </div>
                                 </div>
--- resources/views/partials/contact.blade.php
+++ resources/views/partials/contact.blade.php
@@ -5,7 +5,7 @@
                 <div class="before-heading">N'hésitez pas à nous contacter pour plus d'information</div>
                 <h2 class="text-transform-none fw-semibold display-4">Vous voulez nous parler ?</h2>
                 <h4 class="fw-light mb-4">Appeler nous au <a href="tel:+(228)93-51-80-32">+(228)93-51-80-32</a></h4>
-                <a href="mailto:contact@todlconsulting.com" class="button button-rounded button-xlarge m-0 text-transform-none fw-semibold ls-0 px-5">Email Us</a>
+                <a href="mailto:contact@todlconsulting.com" class="button button-rounded button-xlarge m-0 text-transform-none fw-semibold ls-0 px-5">Ecrivez-nous</a>
             </div>
         </div>
     </div>
